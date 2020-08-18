c6f180c6cb0a47f2e1edb5d2b3fff67b071867fd
iamnotedible <hehonglu123@126.com>
2020-08-18 13:58:50 -0400

    example

--- Examples/turtlebot.py
+++ Examples/turtlebot.py
@@ -1,5 +1,4 @@
 import turtle
-import time
 
 screen = turtle.Screen()
 t1 = turtle.Turtle()
@@ -8,7 +7,7 @@ screen.bgcolor("lightblue")
 
 
 
-def drive(turtlebot,move_speed,turn_speed):            #Drive function, update new position, this is the one referred in definition
+def drive(turtlebot,move_speed,turn_speed):  #Drive function, update new position, this is the one referred in definition
 
 	turtlebot.forward(move_speed)
 	turtlebot.left(turn_speed)
@@ -22,9 +21,7 @@ def setpose(turtlebot,x,y,angle):            #set a new pose for turtlebot
 
 
 for i in range(50):
-	now=time.time()
 	drive(t1,10,0)
 t1.pencolor("red")
 for i in range(50):
-	now=time.time()
 	drive(t1,10,i)
